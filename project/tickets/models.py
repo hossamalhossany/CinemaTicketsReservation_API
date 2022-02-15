b73054964a037948204717cec331f511c51018a0
hossam <hossam@hossam.com>
2022-02-15 13:48:47 +0200

    3 ** 15-2-2022 ** 1:48 PM -- work

--- project/tickets/models.py
+++ project/tickets/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-# hossam here
+
+
 # Create your models here.
+# guest - movie -- reservation
+
+class Movie(models.Model):
+    hall = models.CharField(max_length=10)
+    movie = models.CharField(max_length=10)
+    date = models.DateField()
+    def __str__(self):
+        return self.hall
+
+
+
+class Guest(models.Model):
+    name = models.CharField(max_length=10)
+    mobile = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest, related_name='reservation', on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='reservation', on_delete=models.CASCADE)
