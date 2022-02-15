6a5daf4c24c010c5557e4a44b2d6c618a8ff840a
hossam <hossam@hossam.com>
2022-02-15 14:44:21 +0200

    4 ** 15-2-2022 ** 2:44 PM -- work

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tickets import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('django/jsonres', views.no_rest_no_model),
+    path('django/no_rest_from__model', views.no_rest_from__model)
 ]
--- project/tickets/views.py
+++ project/tickets/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from .models import Guest, Movie, Reservation
+
 
 # Create your views here.
+
+# without rest framework and  without models
+
+def no_rest_no_model(request):
+    guests = [
+        {
+            'id': 1,
+            'name': 'hossam',
+            'mobile': 123456789
+        },
+        {
+            'id': 1,
+            'name': 'yahia',
+            'mobile': 9865428
+        }
+    ]
+    return JsonResponse(guests, safe=False)
+
+# without rest framework and from  models
+def no_rest_from__model(request):
+    data = Guest.objects.all()
+    response = {
+        'guests': list(data.values('name', 'mobile'))
+    }
+    return JsonResponse(response, safe=False)
