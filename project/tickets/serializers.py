40fd7433004b241f1828f6e80ac233bff12e4e48
hossamalhossany <hossam.2030@gmail.com>
2022-02-16 12:16:53 +0200

    7 ** 16-2-2022 ** 12:16 PM - home

--- project/tickets/serializers.py
+++ project/tickets/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from tickets.models import Guest, Movie, Reservation
+from .models import Guest, Movie, Reservation
 
 
 class MovieSerializer(serializers.ModelSerializer):
